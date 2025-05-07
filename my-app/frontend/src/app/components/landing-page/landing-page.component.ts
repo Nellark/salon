7d3357e0a4006b11fc2c49960a9f1be1aea09f6d
Nellark <nelakulati@gmail.com>
2025-05-07 16:29:47 +0200

    added

--- my-app/frontend/src/app/components/landing-page/landing-page.component.ts
+++ my-app/frontend/src/app/components/landing-page/landing-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { HomeComponent } from '../home/home.component';
+import { DisplayComponent } from '../display/display.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
