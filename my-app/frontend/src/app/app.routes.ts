020d117870527017ae984f6b4c6ac8cdc9bb0f14
Nellark <nelakulati@gmail.com>
2025-05-09 08:24:45 +0200

    changed stying on the landing page

--- my-app/frontend/src/app/app.routes.ts
+++ my-app/frontend/src/app/app.routes.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './components/home/home.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { DisplayComponent } from './components/display/display.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent, title: 'Landing' },
   { path: 'home', component: HomeComponent, title: 'Home' },
   {path: 'display', component: DisplayComponent, title: 'Display'},
   {path: 'nav', component: NavBarComponent, title: 'Navbar'},
-];
+  {path: 'footer', component: FooterComponent, title: 'Footer'}
+]; 
 
 
 
--- my-app/frontend/src/app/components/landing-page/landing-page.component.ts
+++ my-app/frontend/src/app/components/landing-page/landing-page.component.ts
@@ -3,12 +3,13 @@ import { RouterLink } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { HomeComponent } from '../home/home.component';
 import { DisplayComponent } from '../display/display.component';
+import { FooterComponent } from '../../footer/footer.component';
 
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [HomeComponent, NavBarComponent],
+  imports: [HomeComponent, NavBarComponent, FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
