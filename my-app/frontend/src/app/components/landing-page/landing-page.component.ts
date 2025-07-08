a09194035d0e33ec0a7da27ca9f7859529f0c6d2
Nellark <nelakulati@gmail.com>
2025-07-08 13:29:17 +0200

    added logo

--- my-app/frontend/src/app/components/landing-page/landing-page.component.ts
+++ my-app/frontend/src/app/components/landing-page/landing-page.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { HomeComponent } from '../home/home.component';
-import { DisplayComponent } from '../display/display.component';
 import { FooterComponent } from '../../footer/footer.component';
-import { BookingComponent } from '../booking/booking.component';
+import { OurServicesComponent } from '../ourServices/ourServices.component';
+
+
 
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [HomeComponent, NavBarComponent, FooterComponent, RouterLink],
+  imports: [HomeComponent, NavBarComponent, FooterComponent, OurServicesComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
---
+++ my-app/frontend/src/app/components/ourServices/ourServices.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ourServices',
+  standalone: true,
+  imports: [],
+  templateUrl: './ourServices.component.html',
+  styleUrl: './ourServices.component.css'
+})
+export class OurServicesComponent {
+
+}
